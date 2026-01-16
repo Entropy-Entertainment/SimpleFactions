99a7a3fdef5418b7441d6ec2ccfabc78230ed3e0
Entropire <158569522+Entropire@users.noreply.github.com>
2026-01-16 20:22:38 +0100

    Added base project

---
+++ src/main/java/net/entropyentertainment/simplefactions/SimpleFactions.java
@@ -0,0 +1,18 @@
+package net.entropyentertainment.simplefactions;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+
+public class SimpleFactions extends JavaPlugin {
+    public SimpleFactions(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+
+    }
+}
